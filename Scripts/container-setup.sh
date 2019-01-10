12cecdf38517abc18a7265dab5d39d28e019aa06
Lewis Denham-Parry <lewis@denhamparry.co.uk>
2019-01-10 16:55:03 +0000

    Wait for RabbitMQ to startup

--- Scripts/container-setup.sh
+++ Scripts/container-setup.sh
@@ -1,4 +1,5 @@
 docker run --name ticketing.messaging --network=ticketing-network -d rabbitmq:3-management
 docker run --name ticketing.mongo --network=ticketing-network -d mongo
 docker run --name ticketing.api --network=ticketing-network -d -p 3000:80 denhamparry/ticketing.api
+sleep 10s
 docker run --name ticketing.worker --network=ticketing-network -d denhamparry/ticketing.worker
