d300d9f72dc1da215c10e912cd218741f12f1ea3
Lewis Denham-Parry <lewis@denhamparry.co.uk>
2019-01-22 17:28:18 +0000

    Add ticketing.web

--- Scripts/container-setup.sh
+++ Scripts/container-setup.sh
@@ -1,5 +1,6 @@
 docker run --name ticketing.messaging --network=ticketing-network -d rabbitmq:3-management
 docker run --name ticketing.mongo --network=ticketing-network -d mongo
-docker run --name ticketing.api --network=ticketing-network -d -p 3000:80 denhamparry/ticketing.api
+docker run --name ticketing.api --network=ticketing-network -d -p 3000:80 denhamparry/ticketing.api:latest
 sleep 10s
-docker run --name ticketing.worker --network=ticketing-network -d denhamparry/ticketing.worker
+docker run --name ticketing.web --network=ticketing-network -d -p 3001:80 denhamparry/ticketing.web:latest
+docker run --name ticketing.worker --network=ticketing-network -d denhamparry/ticketing.worker:latest
