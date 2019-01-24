de89d782d406230008eeb9bc5e057173b2fcd146
Lewis Denham-Parry <lewis@denhamparry.co.uk>
2019-01-24 15:45:55 +0000

    Add ticketing.worker.api

--- Scripts/container-setup.sh
+++ Scripts/container-setup.sh
@@ -4,3 +4,4 @@ docker run --name ticketing.api --network=ticketing-network -d -p 3000:80 denham
 sleep 10s
 docker run --name ticketing.web --network=ticketing-network -d -p 3001:80 denhamparry/ticketing.web:latest
 docker run --name ticketing.worker --network=ticketing-network -d denhamparry/ticketing.worker:latest
+docker run --name ticketing.worker.api --network=ticketing-network -p 3002:80 -d denhamparry/ticketing.worker.api:latest
